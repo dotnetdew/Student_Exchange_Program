5dd5c8baea8d4fc22116b9058efe0471f8bfc036
Avtech <Avtech@ATBM214-09>
2024-07-08 17:22:33 +0500

    resend confirm email bug has been fixed

--- StudentExchange.Wiut.Web/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
+++ StudentExchange.Wiut.Web/Areas/Identity/Pages/Account/ResendEmailConfirmation.cshtml.cs
@@ -72,15 +72,19 @@ namespace StudentExchange.Wiut.Web.Areas.Identity.Pages.Account
             var userId = await _userManager.GetUserIdAsync(user);
             var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
             code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
             var callbackUrl = Url.Page(
                 "/Account/ConfirmEmail",
                 pageHandler: null,
                 values: new { userId = userId, code = code },
                 protocol: Request.Scheme);
+
+            //var encodedCallbackUrl = HtmlEncoder.Default.Encod    e(callbackUrl);
+
             await _emailSender.SendEmailAsync(
                 Input.Email,
                 "Confirm your email",
-                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                $"Please confirm your account by <a href='{callbackUrl}'>clicking here</a>.");
 
             ModelState.AddModelError(string.Empty, "Verification email sent. Please check your email.");
             return Page();
