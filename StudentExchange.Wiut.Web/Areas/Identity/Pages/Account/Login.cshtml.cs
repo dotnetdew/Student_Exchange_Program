52b537aeae6d6a00479e11fd5f07fa07ceb20352
Avtech <Avtech@ATBM214-09>
2024-07-09 15:25:17 +0500

    application checklist view is done

--- StudentExchange.Wiut.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ StudentExchange.Wiut.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -130,7 +130,7 @@ namespace StudentExchange.Wiut.Web.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt. Maybe your account not confirmed. Press resend email confirmation");
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
                     return Page();
                 }
             }
--- StudentExchange.Wiut.Web/Controllers/StudentsController.cs
+++ StudentExchange.Wiut.Web/Controllers/StudentsController.cs
@@ -1,17 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StudentExchange.Wiut.Web.Models;
+using System.Security.Claims;
 
-namespace StudentExchange.Wiut.Web.Controllers
+namespace StudentExchange.Wiut.Web.Controllers;
+
+[Authorize]
+public class StudentsController : Controller
 {
-    public class StudentsController : Controller
-    {
-        public IActionResult MyApplications()
-        {
-            return View();
-        }
+    private readonly UserManager<Student> _userManager;
 
-        public IActionResult ApplicationChecklist()
+    public StudentsController(UserManager<Student> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public IActionResult MyApplications()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> ApplicationChecklist()
+    {
+        var user_id = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+        var user = await _userManager.FindByIdAsync(user_id);
+
+        return View(user);
+    }
+
+    [HttpGet]
+    public IActionResult GetCountries()
+    {
+        using (var client =  new HttpClient())
         {
-            return View();
+            var response = client.GetAsync("https://country.io/names.json").Result;
+            response.EnsureSuccessStatusCode();
+            var data = response.Content.ReadAsStringAsync().Result;
+            return Json(data);
         }
     }
 }
