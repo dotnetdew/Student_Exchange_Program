2579df1d40f05c7c4d8fb3a67e78f4d665e0b5a2
Avtech <Avtech@ATBM214-09>
2024-07-17 14:12:49 +0500

    some ui changes

--- StudentExchange.Wiut.Web/Controllers/StudentsController.cs
+++ StudentExchange.Wiut.Web/Controllers/StudentsController.cs
@@ -79,6 +79,17 @@ public class StudentsController : Controller
     }
 
     [HttpGet]
+    public IActionResult DisplayPassport(string studentId)
+    {
+        var personalDetail = _personalDetailsRepo.GetByStudentId(studentId);
+
+        if (personalDetail != null && personalDetail.PassportFile != null)
+            return File(personalDetail.PassportFile, personalDetail.PassportFileType, personalDetail.PassportFileName);
+        else
+            return new EmptyResult();
+    }
+
+    [HttpGet]
     public IActionResult SavePersonalDetails(string studentId)
     {
         var existingPersonalDetails = _personalDetailsRepo.GetByStudentId(studentId);
@@ -473,6 +484,7 @@ public class StudentsController : Controller
     {
         var student = _studentdRepository.GetById(studentId);
         var existingSubmission = _submissionRepo.GetByStudentId(studentId);
+        var personalDetail = _personalDetailsRepo.GetByStudentId(studentId);
 
         var submissionVM = new CreateSubmissionVM()
         {
@@ -485,6 +497,8 @@ public class StudentsController : Controller
             submissionVM.AgreeToStatements = existingSubmission.AgreeToStatements;
             submissionVM.SubmissionCreated = existingSubmission.SubmissionCreated;
         }
+
+        ViewBag.PassportFile = personalDetail.PassportFile;
 
         return View(submissionVM);
     }
@@ -518,77 +532,65 @@ public class StudentsController : Controller
 
             _submissionRepo.Save();
 
-            string emailBody = @"
+            //string emailBody = @"
+            //<!DOCTYPE html>
+            //<html>
+            //<head>
+            //    <title>Student Exchange Offer</title>
+            //</head>
+            //<body>
+            //    <img src='cid:email_west.png' alt='WIUT Logo' />
+            //    <p>Dear {StudentName},</p>
+            //    <p>It is our pleasure to inform you that you have been accepted onto the Westminster University in Tashkent Exchange Programme.</p>
+            //    <h3>Exchange Programme Details:</h3>
+            //    <p>Academic Year: 2024/25</p>
+            //    <p>Duration: One semester</p>
+            //    <p>Key Dates:</p>
+            //    <ul>
+            //        <li>Orientation Week: 9 – 13 September 2024</li>
+            //        <li>Semester 1 duration: 16 September – 27 December 2024</li>
+            //    </ul>
+            //    <h3>What to do next?</h3>
+            //    <h4>Pre-Arrival Information</h4>
+            //    <p>The International Office will be sending you a pre-arrival e-mail to assist you with preparing for your upcoming semester at WIUT shortly. The information covered will include information to help you prepare for the accommodation and visa information student life in Uzbekistan and at WIUT.</p>
+            //    <h4>Terms and Conditions</h4>
+            //    <p>You should note details of our Inbound Student Exchange Terms and Conditions which can be found at the bottom of the page.</p>
+            //    <h4>Module and Results</h4>
+            //    <p>There is a set module pathway for exchange students nominated on the Economics subject area. You will require to contact either the Student Exchange Coordinator of your home campus or WIUT Course Leader of your subject area for advice on module selection.</p>
+            //    <p>Please note that as a semester-long student you will not receive semester 1 results until the result release date after semester 2.</p>
+            //    <h4>Disability Learning Support</h4>
+            //    <p>WIUT welcomes students with conditions or disabilities. We have an Inclusivity and Diversity Officer who can provide confidential support to help you make the most of your studies during your study abroad experience.</p>
+            //    <p>If you did not declare a disability at the time of application and now wish to register please contact us via email on sang@wiut.uz so that we can provide you with further information on how to register.</p>
+            //    <h4>Comprehensive Medical and Travel Insurance</h4>
+            //    <p>All students should ensure that they have comprehensive private medical and travel insurance to cover their time in Uzbekistan.</p>
+            //    <p>We look forward to welcoming you to WIUT!</p>
+            //    <p>Kind regards,</p>
+            //    <p>Student Records</p>
+            //    <p>Academic Registrar’s Office</p>
+            //    <p>Westminster International University in Tashkent</p>
+            //</body>
+            //</html>";
+
+            string StudentName = student.ForeName + " " + student.FamilyName;
+            string emailBody = $@"
             <!DOCTYPE html>
             <html>
             <head>
-                <title>Student Exchange Offer</title>
+                <title>Submission of application</title>
             </head>
             <body>
-                <img src='cid:email_west.png' alt='WIUT Logo' />
                 <p>Dear {StudentName},</p>
-                <p>It is our pleasure to inform you that you have been accepted onto the Westminster University in Tashkent Exchange Programme.</p>
-                <h3>Exchange Programme Details:</h3>
-                <p>Academic Year: 2024/25</p>
-                <p>Duration: One semester</p>
-                <p>Key Dates:</p>
-                <ul>
-                    <li>Orientation Week: 9 – 13 September 2024</li>
-                    <li>Semester 1 duration: 16 September – 27 December 2024</li>
-                </ul>
-                <h3>What to do next?</h3>
-                <h4>Pre-Arrival Information</h4>
-                <p>The International Office will be sending you a pre-arrival e-mail to assist you with preparing for your upcoming semester at WIUT shortly. The information covered will include information to help you prepare for the accommodation and visa information student life in Uzbekistan and at WIUT.</p>
-                <h4>Terms and Conditions</h4>
-                <p>You should note details of our Inbound Student Exchange Terms and Conditions which can be found at the bottom of the page.</p>
-                <h4>Module and Results</h4>
-                <p>There is a set module pathway for exchange students nominated on the Economics subject area. You will require to contact either the Student Exchange Coordinator of your home campus or WIUT Course Leader of your subject area for advice on module selection.</p>
-                <p>Please note that as a semester-long student you will not receive semester 1 results until the result release date after semester 2.</p>
-                <h4>Disability Learning Support</h4>
-                <p>WIUT welcomes students with conditions or disabilities. We have an Inclusivity and Diversity Officer who can provide confidential support to help you make the most of your studies during your study abroad experience.</p>
-                <p>If you did not declare a disability at the time of application and now wish to register please contact us via email on sang@wiut.uz so that we can provide you with further information on how to register.</p>
-                <h4>Comprehensive Medical and Travel Insurance</h4>
-                <p>All students should ensure that they have comprehensive private medical and travel insurance to cover their time in Uzbekistan.</p>
-                <p>We look forward to welcoming you to WIUT!</p>
-                <p>Kind regards,</p>
-                <p>Student Records</p>
-                <p>Academic Registrar’s Office</p>
+                <p>Your Student Exchange Programme application to the Westminster International University in Tashkent has been submitted. We will be in touch regarding the outcome of your application.</p>
+                <p>As always, please do not hesitate to contact us via email if any other issues arise concerning your application.</p>
+                <p>With best wishes,</p>
+                <p>Sharon (Seow Yee) Ang<br>
+                Senior Officer on International Studies<br>
+                <a href='mailto:sang@wiut.uz'>sang@wiut.uz</a></p>
                 <p>Westminster International University in Tashkent</p>
             </body>
             </html>";
 
-            string studentName = student.ForeName + " " + student.FamilyName;
-            emailBody = emailBody.Replace("{StudentName}", studentName);
-
-            string logoPath = Path.Combine(_hostingEnvironment.WebRootPath, "images", "email_west.png");
-
-            // Create the alternate view for the email body
-            AlternateView htmlView = AlternateView.CreateAlternateViewFromString(emailBody, null, MediaTypeNames.Text.Html);
-
-            // Add the image to the email
-            LinkedResource logo = new LinkedResource(logoPath, MediaTypeNames.Image.Png)
-            {
-                ContentId = "email_west.png"
-            };
-            htmlView.LinkedResources.Add(logo);
-
-
-            MailMessage message = new MailMessage();
-            message.From = new MailAddress("exchange@wiut.uz", "Westminster University in Tashkent");
-            message.To.Add(vm.EmailAddress);
-            message.Subject = "Submission";
-            message.Body = string.Empty;
-            message.IsBodyHtml = true;
-            message.AlternateViews.Add(htmlView);
-
-            SmtpClient smtpClient = new SmtpClient();
-            smtpClient.Host = "klms.wiut.uz";
-            smtpClient.Port = 27;
-            smtpClient.EnableSsl = true;
-            smtpClient.Credentials = new NetworkCredential("exchange@wiut.uz", "2873KLMS365");
-            smtpClient.Timeout = 30000;
-
-            smtpClient.Send(message);
+            _emailSender.SendEmailAsync(vm.EmailAddress, "Submission of application", emailBody);
 
             ViewBag.Saved = "true";
 
